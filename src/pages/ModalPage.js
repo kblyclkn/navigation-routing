c345bbcf7416359aba631e3358ede9296c0d8085
kblyclkn <105425261+kblyclkn@users.noreply.github.com>
2023-04-04 13:25:06 +0300

    modal create portal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../components/Modal";
+import Button from "../components/Button";
 
 function ModalPage() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
   return (
     <div>
-      <Modal />
+        <Button onClick={handleClick} primary>Open Modal</Button>
+      { showModal && <Modal />}
     </div>
   );
 }
